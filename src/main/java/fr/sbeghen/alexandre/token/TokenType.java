7ef09dd215a19352ba7b2c640a1fe457e6706b0e
Alexandre-SBEGHEN <alexandre.sbeghen-magnin@etu.univ-amu.fr>
2026-08-29 14:53:14 +0200

    tests(expression): Tests liés aux retours de tokenize()
    Deux tests ajoutés :
    
    - Une opération simple (4 + 2 * 3)
    - Une opération avec parenthèses (6 / 2 * (1 + 2))

--- src/main/java/fr/sbeghen/alexandre/token/TokenType.java
+++ src/main/java/fr/sbeghen/alexandre/token/TokenType.java
@@ -2,8 +2,19 @@ package fr.sbeghen.alexandre.token;
 
 /**
  * Représente les différents types qu'un token peut prendre.
+ *
+ * @see Token
  */
 public enum TokenType {
+    /** Parenthèse ouvrante '(' */
+    LEFT,
+
+    /** Parenthèse fermante ')' */
+    RIGHT,
+
+    /** Opération (+-* etc) */
     OPERATION,
+
+    /** Nombre réel */
     NUMBER
 }
