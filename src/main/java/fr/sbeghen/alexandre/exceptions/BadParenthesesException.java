dddf23deeaf66fbfd69bc54925ecaf8147481ece
Alexandre-SBEGHEN <alexandre.sbeghen-magnin@etu.univ-amu.fr>
2026-08-29 12:32:58 +0200

    docs(exceptions): Documentation des exceptions créées

--- src/main/java/fr/sbeghen/alexandre/exceptions/BadParenthesesException.java
+++ src/main/java/fr/sbeghen/alexandre/exceptions/BadParenthesesException.java
@@ -1,5 +1,9 @@
 package fr.sbeghen.alexandre.exceptions;
 
+/**
+ * Exception qui a lieu lorsqu'une expression mathématique
+ * présente un mauvais parenthésage, la rendant donc invalide.
+ */
 public class BadParenthesesException extends RuntimeException {
     public BadParenthesesException(String message) {
         super(message);
--- src/main/java/fr/sbeghen/alexandre/exceptions/IllegalCharacterException.java
+++ src/main/java/fr/sbeghen/alexandre/exceptions/IllegalCharacterException.java
@@ -1,5 +1,10 @@
 package fr.sbeghen.alexandre.exceptions;
 
+/**
+ * Exception qui a lieu lorsqu'un caractère non reconnu
+ * est présent dans une expression mathématique, la
+ * rendant donc invalide.
+ */
 public class IllegalCharacterException extends RuntimeException {
     public IllegalCharacterException(String message) {
         super(message);
