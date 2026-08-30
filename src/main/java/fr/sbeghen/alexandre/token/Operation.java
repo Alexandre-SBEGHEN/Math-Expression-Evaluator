acc3ce71cb9105327663d297b2dba56ba4a1aa89
Alexandre-SBEGHEN <alexandre.sbeghen-magnin@etu.univ-amu.fr>
2026-08-30 15:19:08 +0200

    feat(Operation): Implémentation de l'enum
    - Mappage de chaque opération à un caractère
    - Méthode pour retrouver l'enum à partir d'un caractère
        - pourra éventuellement nécessiter des tests

--- src/main/java/fr/sbeghen/alexandre/token/Operation.java
+++ src/main/java/fr/sbeghen/alexandre/token/Operation.java
@@ -4,15 +4,20 @@ package fr.sbeghen.alexandre.token;
  * Représente les opérations arithmétiques supportées par l'évaluateur.
  */
 public enum Operation {
-    /** Addition (+) */
-    PLUS,
+    PLUS('+'),
+    MINUS('-'),
+    TIMES('*'),
+    DIV('/');
 
-    /** Soustraction (-) */
-    MINUS,
+    public final char character;
+    Operation(char character) { this.character = character; }
 
-    /** Multipication (*) */
-    TIMES,
-
-    /** Division (/). Lève une exception si le diviseur vaut zéro. */
-    DIV
+    public static Operation fromChar(char c) {
+        for (Operation op: values()) {
+            if (op.character == c) {
+                return op;
+            }
+        }
+        throw new IllegalArgumentException(String.format("Invalid operator : '%c'", c));
+    }
 }
