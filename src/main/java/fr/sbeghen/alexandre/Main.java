04be88c552e23b56c396077d5901a2b4013ecb75
Alexandre-SBEGHEN <alexandre.sbeghen-magnin@etu.univ-amu.fr>
2026-08-30 19:40:49 +0200

    misc: Tests visuels dans le main

--- src/main/java/fr/sbeghen/alexandre/Main.java
+++ src/main/java/fr/sbeghen/alexandre/Main.java
@@ -1,12 +1,20 @@
 package fr.sbeghen.alexandre;
 
 import fr.sbeghen.alexandre.token.*;
+import java.util.ArrayList;
 
 /**
  * Point d'entrée du programme.
  */
 public class Main {
     static void main(String[] args) {
+        Expression exp = new Expression("6 / 2 * (1 + 2)");
+        System.out.println("Expression: '" + exp + "'");
 
+        ArrayList<Token> tokens = exp.tokenize();
+        System.out.print("Tokens: ( ");
+        for (Object t: tokens.toArray())
+            System.out.print(((Token)t).type() + "\t");
+        System.out.println(")");
     }
 }
