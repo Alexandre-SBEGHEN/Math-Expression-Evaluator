524ecbdf657524d2473e9589c520c9675de440c6
Alexandre-SBEGHEN <alexandre.sbeghen-magnin@etu.univ-amu.fr>
2026-08-31 12:19:12 +0200

    tests(AbstractSyntaxTree): Création de la classe de tests

---
+++ src/test/java/fr/sbeghen/alexandre/parsing/AbstractSyntaxTreeTest.java
@@ -0,0 +1,28 @@
+package fr.sbeghen.alexandre.parsing;
+
+import fr.sbeghen.alexandre.tokenization.Expression;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Classe de test de AbstractSyntaxTree.
+ * <p>
+ * <strong>Note :</strong> ne prend pas en compte les méthodes triviales
+ * telles que les guetters, setters, toString, etc. Seulement les
+ * implémentations propres au programme.
+ *
+ * @see AbstractSyntaxTree
+ */
+public class AbstractSyntaxTreeTest {
+    /*
+     * ----------------------- Tests de evaluate() ----------------------------
+     */
+
+    /* ----- Exceptions ----- */
+
+
+}
