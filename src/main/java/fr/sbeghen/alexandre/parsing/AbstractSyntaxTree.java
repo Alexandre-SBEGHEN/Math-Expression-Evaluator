986f362e5510e15dd6c80cdc7e94c02fc9259331
Alexandre-SBEGHEN <alexandre.sbeghen-magnin@etu.univ-amu.fr>
2026-08-31 12:02:43 +0200

    feat(AbstractSyntaxTree): Sinatures de applyOperation() et evaluate()
    La documentation est également rédigée.

--- src/main/java/fr/sbeghen/alexandre/parsing/AbstractSyntaxTree.java
+++ src/main/java/fr/sbeghen/alexandre/parsing/AbstractSyntaxTree.java
@@ -1,5 +1,7 @@
 package fr.sbeghen.alexandre.parsing;
 
+import fr.sbeghen.alexandre.exception.DivisionByZeroException;
+import fr.sbeghen.alexandre.tokenization.Operation;
 import fr.sbeghen.alexandre.tokenization.Token;
 
 /**
@@ -49,4 +51,45 @@ public class AbstractSyntaxTree {
     public Token getRoot() { return root; }
     public Token getLeft() { return left; }
     public Token getRight() { return right; }
+
+    /**
+     * Applique une opération entre un ou deux opérandes.
+     *
+     * @param operation L'opération à effectuer.
+     * @param a Premier opérande.
+     * @param b Second opérande.
+     * @return Résultat de l'opération
+     *
+     * @throws DivisionByZeroException Si l'opération est une division,
+     * et que le diviseur (second opérande) est nul.
+     */
+    public double applyOperation(Operation operation, double a, double b) {
+        return 0;
+    }
+
+    /**
+     * Fonction résursive d'évaluation d'un <i>AST</i>.
+     * <p>
+     * Celle-ci prend en paramètre le nœud par lequel
+     * commencer la récursion.
+     *
+     * @param node Le nœud de départ
+     * @return Le résultat de l'évaluation.
+     */
+    public double evaluate(AbstractSyntaxTree node) {
+        return 0;
+    }
+    /**
+     * Fonction résursive d'évaluation d'un <i>AST</i>.
+     * <p>
+     * Signature sans paramètre, on commence donc
+     * par le nœud lui-même.
+     *
+     * @return Le résultat de l'évaluation.
+     *
+     * @see evaluate(AbstractSyntaxTree)
+     */
+    public double evaluate() {
+        return evaluate(this);
+    }
 }
