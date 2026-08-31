7830231925afe6dac38c3024110d1f4b6fa97e5c
Alexandre-SBEGHEN <alexandre.sbeghen-magnin@etu.univ-amu.fr>
2026-08-31 17:37:49 +0200

    feat(IllegalCharacterException): L'exception hérite de ExpressionException

--- src/main/java/fr/sbeghen/alexandre/exception/IllegalCharacterException.java
+++ src/main/java/fr/sbeghen/alexandre/exception/IllegalCharacterException.java
@@ -1,11 +1,15 @@
 package fr.sbeghen.alexandre.exception;
 
 /**
+ * Sous-exception de ExpressionException.
+ * <p>
  * Exception qui a lieu lorsqu'un caractère invalide
  * est présent dans une expression mathématique, la
  * rendant donc elle-même invalide.
+ *
+ * @see ExpressionException
  */
-public class IllegalCharacterException extends RuntimeException {
+public class IllegalCharacterException extends ExpressionException {
     public IllegalCharacterException(String message) {
         super(message);
     }
