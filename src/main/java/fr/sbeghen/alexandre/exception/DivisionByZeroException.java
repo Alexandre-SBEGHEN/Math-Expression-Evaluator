bb7675fdf374e85926453d5e397b4ec67120f298
Alexandre-SBEGHEN <alexandre.sbeghen-magnin@etu.univ-amu.fr>
2026-08-31 17:29:18 +0200

    feat(DivisionByZeroException): L'exception hérite de ArithmeticException

--- src/main/java/fr/sbeghen/alexandre/exception/DivisionByZeroException.java
+++ src/main/java/fr/sbeghen/alexandre/exception/DivisionByZeroException.java
@@ -4,7 +4,7 @@ package fr.sbeghen.alexandre.exception;
  * Exception qui a lieu lors du parsing d'un AST, lorsqu'une division
  * est tentée, mais que le diviseur est nul.
  */
-public class DivisionByZeroException extends RuntimeException {
+public class DivisionByZeroException extends ArithmeticException {
     public DivisionByZeroException(String message) {
         super(message);
     }
