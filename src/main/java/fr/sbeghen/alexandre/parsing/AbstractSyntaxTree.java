4f31290d4a03a689ff71ce6054bd9a520b6bf26a
Alexandre-SBEGHEN <alexandre.sbeghen-magnin@etu.univ-amu.fr>
2026-08-31 11:03:32 +0200

    feat(parsing): Classe de l'AST avec constructeurs et getters

---
+++ src/main/java/fr/sbeghen/alexandre/parsing/AbstractSyntaxTree.java
@@ -0,0 +1,52 @@
+package fr.sbeghen.alexandre.parsing;
+
+import fr.sbeghen.alexandre.token.Token;
+
+/**
+ * Arbre binaire de l'expression mathématique.
+ * Possède une racine, ainsi que d'éventuels enfants
+ * gauche et droit.
+ * <p>
+ * Contient également les méthodes permettant
+ * l'évaluation de cet arbre.
+ */
+public class AbstractSyntaxTree {
+    private final Token root;
+    private final Token left;
+    private final Token right;
+
+    /**
+     * Premier constructeur de l'<i>AST</i>.
+     * <p>
+     * Il possède une racine ainsi que <strong>deux</strong> enfants gauche et droit.
+     */
+    public AbstractSyntaxTree(Token root, Token left, Token right) {
+        this.root = root;
+        this.left = left;
+        this.right = right;
+    }
+    /**
+     * Second constructeur de l'<i>AST</i>.
+     * <p>
+     * Il possède une racine ainsi qu'<strong>un seul</strong> enfant qui est le gauche.
+     */
+    public AbstractSyntaxTree(Token root, Token left) {
+        this.root = root;
+        this.left = left;
+        right = null;
+    }
+    /**
+     * Troisième et dernier constructeur de l'<i>AST</i>.
+     * <p>
+     * Il possède une racine et <strong>aucun</strong> enfant.
+     */
+    public AbstractSyntaxTree(Token root) {
+        this.root = root;
+        left = null;
+        right = null;
+    }
+
+    public Token getRoot() { return root; }
+    public Token getLeft() { return left; }
+    public Token getRight() { return right; }
+}
