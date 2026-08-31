100995106fec487128dcdca7ed177905f5566856
Alexandre-SBEGHEN <alexandre.sbeghen-magnin@etu.univ-amu.fr>
2026-08-31 17:37:17 +0200

    feat(BadParenthesesException): L'exception hérite de ExpressionException

--- src/main/java/fr/sbeghen/alexandre/exception/BadParenthesesException.java
+++ src/main/java/fr/sbeghen/alexandre/exception/BadParenthesesException.java
@@ -1,10 +1,14 @@
 package fr.sbeghen.alexandre.exception;
 
 /**
+ * Sous-exception de ExpressionException.
+ * <p>
  * Exception qui a lieu lorsqu'une expression mathématique
  * présente un mauvais parenthésage, la rendant donc invalide.
+ *
+ * @see ExpressionException
  */
-public class BadParenthesesException extends RuntimeException {
+public class BadParenthesesException extends ExpressionException {
     public BadParenthesesException(String message) {
         super(message);
     }
