7c3416d236b4cb7f5bc3118aae5881bc93ac5058
Alexandre-SBEGHEN <alexandre.sbeghen-magnin@etu.univ-amu.fr>
2026-09-02 21:40:35 +0200

    feat(Parser): Création classe avec signatures des méthodes
    Je vais séparer le parser de l'AST car cela aurait plus de sens de
    le faire ainsi, et pas juste de tout mettre dans la classe de l'arbre.

---
+++ src/main/java/fr/sbeghen/alexandre/parsing/Parser.java
@@ -0,0 +1,59 @@
+package fr.sbeghen.alexandre.parsing;
+
+import java.util.Collections;
+import java.util.ArrayList;
+import fr.sbeghen.alexandre.tokenization.Token;
+
+/**
+ * Classe du parser.
+ * <p>
+ * C'est la passerelle entre la liste des tokens
+ * et l'<i>AST</i>.
+ */
+public class Parser {
+    private final ArrayList<Token> tokens;
+
+    /**
+     * Constructeur du parser.
+     * <p>
+     * Utilise la liste des tokens à l'envers
+     * pour permettre une récupération du suivant
+     * en O(1) (opération <code>pop</code>)
+     *
+     * @param tokens La liste des tokens.
+     */
+    public Parser(ArrayList<Token> tokens) {
+        this.tokens = new ArrayList<>(tokens);
+        Collections.reverse(this.tokens);
+    }
+
+    /**
+     * Construit un <i>AST</i> représentant l'expression
+     * mathématique.
+     *
+     * @return L'arbre de l'expression.
+     */
+    public AbstractSyntaxTree constructTree() {
+        return null;
+    }
+
+    /**
+     * Regarde quel est le token suivant, le retire
+     * de la liste, puis le renvoie.
+     *
+     * @return Le token suivant, null s'il n'y en a pas.
+     */
+    public Token next() {
+        return null;
+    }
+
+    /**
+     * Regarde quel est le token suivant, puis le
+     * renvoie sans le supprimer de la liste
+     *
+     * @return Le token suivant, null s'il n'y en a pas.
+     */
+    public Token peek() {
+        return null;
+    }
+}
