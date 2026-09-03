503f0bebd87d7e78300cb20301e7b43527af61d0
Alexandre-SBEGHEN <alexandre.sbeghen-magnin@etu.univ-amu.fr>
2026-09-03 21:56:24 +0200

    refactor(BindingPower): Noms attributs en lbp et rbp

--- src/main/java/fr/sbeghen/alexandre/parsing/BindingPower.java
+++ src/main/java/fr/sbeghen/alexandre/parsing/BindingPower.java
@@ -5,7 +5,7 @@ package fr.sbeghen.alexandre.parsing;
  * est une valeur numérique associée à chaque opérateur qui
  * détermine sa priorité et son associativité.
  *
- * @param left Priorité à gauche.
- * @param right Priorité à droite.
+ * @param lbp <i>Left binding power</i> : la force avec laquelle l'opérateur s'accroche à l'opérande à sa gauche.
+ * @param rbp <i>Right binding power</i> : la force avec laquelle il s'accroche à l'opérande à sa droite
  */
-public record BindingPower(double left, double right) {}
+public record BindingPower(double lbp, double rbp) {}
