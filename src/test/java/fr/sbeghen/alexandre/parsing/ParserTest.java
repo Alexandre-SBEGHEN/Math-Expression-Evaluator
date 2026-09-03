f6856d5142c73548ae2f4994abd08a2b81525772
Alexandre-SBEGHEN <alexandre.sbeghen-magnin@etu.univ-amu.fr>
2026-09-03 13:55:21 +0200

    tests(Parser): Transfert des méthodes dans la classe

---
+++ src/test/java/fr/sbeghen/alexandre/parsing/ParserTest.java
@@ -0,0 +1,127 @@
+package fr.sbeghen.alexandre.parsing;
+
+import fr.sbeghen.alexandre.tokenization.Operation;
+import fr.sbeghen.alexandre.tokenization.Token;
+import fr.sbeghen.alexandre.tokenization.TokenType;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ParserTest {
+
+    /**
+     * Vérifie pour une opération simple, si le bon
+     * arbre est construit.
+     */
+    @Disabled
+    @Test
+    void constructTreeSimpleOperation() {
+        // Construction de l'opération '2 + 3'
+        ArrayList<Token> tokens = new ArrayList<>();
+        tokens.add(new Token(TokenType.NUMBER, 2.0));
+        tokens.add(new Token(TokenType.OPERATION, Operation.PLUS.ordinal()));
+        tokens.add(new Token(TokenType.NUMBER, 3.0));
+
+        Parser parser = new Parser(tokens);
+
+        Tree expectedTree = new Tree(
+                new Token(TokenType.OPERATION, Operation.PLUS.ordinal()),
+                new Tree(new Token(TokenType.NUMBER, 2.0)),
+                new Tree(new Token(TokenType.NUMBER, 3.0))
+        );
+
+        assertEquals(expectedTree, parser.constructTree());
+    }
+
+    /**
+     * Vérifie pour une opération intermédiaire,
+     * si le bon arbre est construit.
+     */
+    @Disabled
+    @Test
+    void constructTreeIntermediateOperation() {
+        // Construction de l'opération '6 / 2 * (1 + 2)'
+        ArrayList<Token> tokens = new ArrayList<>();
+        tokens.add(new Token(TokenType.NUMBER, 6.0));
+        tokens.add(new Token(TokenType.OPERATION, Operation.DIV.ordinal()));
+        tokens.add(new Token(TokenType.NUMBER, 2.0));
+        tokens.add(new Token(TokenType.OPERATION, Operation.TIMES.ordinal()));
+        tokens.add(new Token(TokenType.LEFT, 0.0));
+        tokens.add(new Token(TokenType.NUMBER, 1.0));
+        tokens.add(new Token(TokenType.OPERATION, Operation.PLUS.ordinal()));
+        tokens.add(new Token(TokenType.NUMBER, 2.0));
+        tokens.add(new Token(TokenType.LEFT, 0.0));
+
+        Parser parser = new Parser(tokens);
+
+        Tree expectedTree = new Tree(
+                new Token(TokenType.OPERATION, Operation.PLUS.ordinal()),
+                new Tree(
+                        new Token(TokenType.OPERATION, Operation.DIV.ordinal()),
+                        new Tree(new Token(TokenType.NUMBER, 6.0)),
+                        new Tree(new Token(TokenType.NUMBER, 2.0))
+                ),
+                new Tree(
+                        new Token(TokenType.OPERATION, Operation.PLUS.ordinal()),
+                        new Tree(new Token(TokenType.NUMBER, 1.0)),
+                        new Tree(new Token(TokenType.NUMBER, 2.0))
+                )
+        );
+
+        assertEquals(expectedTree, parser.constructTree());
+    }
+
+    /**
+     * Vérifie pour une opération complexe,
+     * si le bon arbre est construit.
+     */
+    @Disabled
+    @Test
+    void constructTreeComplexOperation() {
+        // Construction de l'opération '-6 / 4 + 1 --2 * 3'
+        ArrayList<Token> tokens = new ArrayList<>();
+        tokens.add(new Token(TokenType.OPERATION, Operation.NEGATE.ordinal()));
+        tokens.add(new Token(TokenType.NUMBER, 6.0));
+        tokens.add(new Token(TokenType.OPERATION, Operation.DIV.ordinal()));
+        tokens.add(new Token(TokenType.NUMBER, 4.0));
+        tokens.add(new Token(TokenType.OPERATION, Operation.PLUS.ordinal()));
+        tokens.add(new Token(TokenType.NUMBER, 1.0));
+        tokens.add(new Token(TokenType.OPERATION, Operation.MINUS.ordinal()));
+        tokens.add(new Token(TokenType.OPERATION, Operation.NEGATE.ordinal()));
+        tokens.add(new Token(TokenType.NUMBER, 2.0));
+        tokens.add(new Token(TokenType.OPERATION, Operation.TIMES.ordinal()));
+        tokens.add(new Token(TokenType.NUMBER, 3.0));
+
+        Parser parser = new Parser(tokens);
+
+        // Une infamie sans nom
+        Tree expectedTree = new Tree(
+                new Token(TokenType.OPERATION, Operation.MINUS.ordinal()),
+                new Tree(
+                        new Token(TokenType.OPERATION, Operation.PLUS.ordinal()),
+                        new Tree(
+                                new Token(TokenType.OPERATION, Operation.DIV.ordinal()),
+                                new Tree(
+                                        new Token(TokenType.OPERATION, Operation.NEGATE.ordinal()),
+                                        new Tree(new Token(TokenType.NUMBER, 6.0))
+                                ),
+                                new Tree(new Token(TokenType.NUMBER, 4.0))
+                        ),
+                        new Tree(new Token(TokenType.NUMBER, 1.0))
+                ),
+                new Tree(
+                        new Token(TokenType.OPERATION, Operation.TIMES.ordinal()),
+                        new Tree(
+                                new Token(TokenType.OPERATION, Operation.NEGATE.ordinal()),
+                                new Tree(new Token(TokenType.NUMBER, 2.0))
+                        ),
+                        new Tree(new Token(TokenType.NUMBER, 3.0))
+                )
+        );
+
+        assertEquals(expectedTree, parser.constructTree());
+    }
+}
